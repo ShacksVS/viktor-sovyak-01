8012c6335e45393153cec7e4de9c9e2260668997
ShacksVS <inode16@gmail.com>
2024-03-17 22:04:33 +0200

    added function basic implementation

---
+++ app/io/input.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def get_user_input():
+    """
+    Prompt the user to enter input and return the input entered by the user.
+
+    Returns:
+        str: The input entered by the user.
+    """
+
+
+def read_input_file(filename):
+    """
+    Read lines from a file and return them as a list of strings.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        list: A list of strings representing the lines read from the file.
+    """
+
+
+def read_input_file_pandas(filename):
+    """
+    Read a file using pandas and return the contents as a DataFrame.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        pandas.DataFrame or None: A DataFrame containing the contents of the file,
+                                  or None if the file is not found.
+    """
---
+++ app/io/output.py
@@ -0,0 +1,27 @@
+def print_to_console(text):
+    """
+    Print the provided text to the console.
+
+    Args:
+        text (str): The text to be printed to the console.
+    """
+
+
+def write_to_file(filename, text):
+    """
+    Write the provided text to the specified file.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to be written to the file.
+    """
+
+
+def write_dataframe_to_csv(filename, dataframe):
+    """
+    Write a pandas DataFrame to a CSV file using default Python methods.
+
+    Args:
+        filename (str): The name of the file to write to.
+        dataframe (pandas.DataFrame): The DataFrame to be written to the file.
+    """
